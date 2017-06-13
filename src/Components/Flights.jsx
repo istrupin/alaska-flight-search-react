87a22b023417df9894a0a5870258a0b83314e510
Igor Strupinskiy <Igor Strupinskiy>
2017-06-12 22:37:51 -0700

    mocked up table

--- src/Components/Flights.jsx
+++ src/Components/Flights.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import SearchBox from './Search_Box.jsx';
+import ResultsTable from './Results_Table.jsx';
+import '../Styles/DataTable.css';
+import '../Styles/DataTableBase.css';
+
+
 
 
 const Flights = () => (
-  <div>
-    <SearchBox />    
-  </div>
-  )
+    <div>
+        <div>
+            <SearchBox />
+        </div>
+
+        <br />
+        <div className="row">
+        <div className="col-md-3 col-md-push-1">
+            <ResultsTable  />
+        </div>
+        </div>
+    </div>
+)
 
 export default Flights;
