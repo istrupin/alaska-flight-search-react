88e9f1d2e47a6450f542f98998c1416bafc56c9e
Igor Strupinskiy <Igor Strupinskiy>
2017-06-21 22:30:20 -0700

    receive props into result table from flight component

--- src/Components/Flights.jsx
+++ src/Components/Flights.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SearchBox from './Search_Box.jsx';
 import ResultsTable from './Results_Table.jsx';
 import '../Styles/DataTable.css';
@@ -7,19 +7,43 @@ import '../Styles/DataTableBase.css';
 
 
 
-const Flights = () => (
-    <div>
-        <div>
-            <SearchBox />
-        </div>
+class Flights extends Component {
 
-        <br />
-        <div className="row">
-        <div className="col-md-3 col-md-push-1">
-            <ResultsTable  />
-        </div>
-        </div>
-    </div>
-)
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            flights:[]
+        };
+
+        this.getFlights();
+    }
+
+    getFlights(){
+        fetch('http://localhost:49868/api/flights').then((res) => res.json())
+        .then((data) => {
+            this.setState({flights: data});
+            //console.log(this.state.flights);
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <SearchBox />
+                </div>
+
+                <br />
+                <div className="row">
+                    <div className="col-md-3 col-md-push-1">
+                        <ResultsTable flights={this.state.flights} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Flights;
--- src/Components/Results_Table.jsx
+++ src/Components/Results_Table.jsx
@@ -5,74 +5,81 @@ import { FixedDataTable, Cell, Column, Table } from 'fixed-data-table';
 
 
 class MyTextCell extends React.Component {
-  render() {
-    const {rowIndex, field, data, ...props} = this.props;
-    return (
-      <Cell {...props}>
-        {data[rowIndex][field]}
-      </Cell>
-    );
-  }
-}
-
-class MyLinkCell extends React.Component {
-  render() {
-    const {rowIndex, field, data, ...props} = this.props;
-    const link = data[rowIndex][field];
-    return (
-      <Cell {...props}>
-        <a href={link}>{link}</a>
-      </Cell>
-    );
-  }
+    render() {
+        const { rowIndex, field, data, ...props } = this.props;
+        return (
+            <Cell {...props}>
+                {data[rowIndex][field]}
+            </Cell>
+        );
+    }
 }
 
 class ResultsTable extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      myTableData: [
-        {name: 'Rylan', email: 'Angelita_Weimann42@gmail.com'},
-        {name: 'Amelia', email: 'Dexter.Trantow57@hotmail.com'},
-        {name: 'Estevan', email: 'Aimee7@hotmail.com'},
-        {name: 'Florence', email: 'Jarrod.Bernier13@yahoo.com'},
-        {name: 'Tressa', email: 'Yadira1@hotmail.com'},
-      ],
-    };
-  }
+        this.state = {
+            myTableData: []
+        };
+        console.log('props are', props);
+    }
 
-  render() {
-    return (
-      <Table
-        rowsCount={this.state.myTableData.length}
-        rowHeight={50}
-        headerHeight={50}
-        width={1000}
-        height={500}>
-        <Column
-          header={<Cell>Name</Cell>}
-          cell={
-            <MyTextCell
-              data={this.state.myTableData}
-              field="name"
-            />
-          }
-          width={200}
-        />
-        <Column
-          header={<Cell>Email</Cell>}
-          cell={
-            <MyLinkCell
-              data={this.state.myTableData}
-              field="email"
-            />
-          }
-          width={200}
-        />
-      </Table>
-    );
-  }
+    componentWillReceiveProps(props) {
+        this.setState({ myTableData: props.flights }) // This will update your component.
+        console.log('updated props are', props);
+        
+    }
+
+    render() {
+        return (
+            <Table
+                rowsCount={this.state.myTableData.length}
+                rowHeight={50}
+                headerHeight={50}
+                width={1000}
+                height={500}>
+                <Column
+                    header={<Cell>From</Cell>}
+                    cell={
+                        <MyTextCell
+                            data={this.state.myTableData}
+                            field="From"
+                        />
+                    }
+                    width={200}
+                />
+                <Column
+                    header={<Cell>Email</Cell>}
+                    cell={
+                        <MyTextCell
+                            data={this.state.myTableData}
+                            field="To"
+                        />
+                    }
+                    width={200}
+                />
+
+                <Column
+                    header={<Cell>Flight Number</Cell>}
+                    cell={
+                        <MyTextCell
+                            data={this.state.myTableData}
+                            field="FlightNumber"
+                        />
+                    }
+                    width={200}
+                />
+            </Table>
+        );
+    }
 }
+
+// const ResultsTable = (props) => {
+//     console.log('props are', props);
+//     return (
+//         <div>hello</div>
+//     )
+// }
 
 export default ResultsTable;
